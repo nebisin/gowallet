521bf5236d85331271862ea6c77a46504faad51e
Salim Bozok <salimbozok@outlook.com>
2021-04-27 22:33:54 +0300

    entry repository and tests

---
+++ model/entry.go
@@ -0,0 +1,80 @@
+package model
+
+type CreateEntryPayload struct {
+	AccountId uint64 `json:"account_id"`
+	Amount    int64  `json:"amount"`
+}
+
+const createEntry = `INSERT INTO entries (account_id, amount) 
+VALUES ($1, $2) 
+RETURNING id, account_id, amount, created_at`
+
+func (r Repository) CreateEntry(arg CreateEntryPayload) (Entry, error) {
+	row := r.db.QueryRow(createEntry, arg.AccountId, arg.Amount)
+	var entry Entry
+	err := row.Scan(
+		&entry.ID,
+		&entry.AccountID,
+		&entry.Amount,
+		&entry.CreatedAt,
+	)
+	return entry, err
+}
+
+const getEntry = `SELECT id, account_id, amount, created_at 
+FROM entries 
+WHERE id = $1 
+LIMIT 1`
+
+func (r Repository) GetEntry(id uint64) (Entry, error) {
+	row := r.db.QueryRow(getEntry, id)
+
+	var entry Entry
+	err := row.Scan(
+		&entry.ID,
+		&entry.AccountID,
+		&entry.Amount,
+		&entry.CreatedAt,
+	)
+
+	return entry, err
+}
+
+type ListEntryParams struct {
+	AccountID uint64 `json:"account_id"`
+	Limit     int32  `json:"limit"`
+	Offset    int32  `json:"offset"`
+}
+
+const listEntry = `SELECT id, account_id, amount, created_at 
+FROM entries 
+WHERE account_id = $1 
+LIMIT $2 OFFSET $3`
+
+func (r Repository) ListEntry(args ListEntryParams) ([]Entry, error) {
+	rows, err := r.db.Query(listEntry, args.AccountID, args.Limit, args.Offset)
+	if err != nil {
+		return nil, err
+	}
+	var items []Entry
+	for rows.Next() {
+		var i Entry
+		err := rows.Scan(
+			&i.ID,
+			&i.AccountID,
+			&i.Amount,
+			&i.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
---
+++ model/entry_test.go
@@ -0,0 +1,67 @@
+package model
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryPayload{
+		AccountId: account.ID,
+		Amount: 100,
+	}
+
+	entry, err := testRepository.CreateEntry(arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, account.ID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestRepository_CreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestRepository_GetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+
+	entry1 := createRandomEntry(t, account)
+	entry2, err := testRepository.GetEntry(entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestRepository_ListEntry(t *testing.T) {
+	account := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntryParams{
+		AccountID: account.ID,
+		Limit: 5,
+		Offset: 5,
+	}
+
+	entries, err := testRepository.ListEntry(arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, entry.AccountID, arg.AccountID)
+	}
+}
